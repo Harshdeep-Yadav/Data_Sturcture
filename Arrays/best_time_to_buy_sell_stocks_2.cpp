2140ea8bbefd3610df319b6d73da213df3b05ec1
Harshdeep Yadav <harshdeepy5708@gmail.com>
2022-07-11 17:03:29 +0530

    inital commit

---
+++ Arrays/best_time_to_buy_sell_stocks_2.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+     int profit=0;
+        for(int i=0;i<prices.size()-1;i++)
+        {
+            if(prices[i+1]>prices[i])
+        {
+            profit=profit+prices[i+1]-prices[i];
+        }
+    }
+        return profit;
+}
+    
+};
+
