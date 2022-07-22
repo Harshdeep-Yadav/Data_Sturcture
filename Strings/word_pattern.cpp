19bc2a0c08ac18035e343008cf212ddeed4e6c29
Harshdeep Yadav <harshdeepy5708@gmail.com>
2022-07-22 16:13:35 +0530

    initial commit

---
+++ Strings/word_pattern.cpp
@@ -0,0 +1,79 @@
+// class Solution {
+// public:
+//     bool wordPattern(string pattern, string s) {
+        
+//         vector<string> v;
+//         int i=0;
+//         string temp="";
+//         while(i<s.size()){
+//             if(s[i]==' '){
+//                 v.push_back(temp);
+//                 temp="";
+//             }
+//             else
+//                 temp+=s[i];
+//              i++; 
+//         }
+//         v.push_back(temp);
+        
+//         if(pattern.size()!=s.size()) return false;
+//         unordered_map<char,int> m;
+//         set<string>st;
+//         for(int i=0;i<pattern.size();i++){
+//             if(m.find(pattern[i])!=m.end()){
+//                if(m[pattern[i]]!=v[i]){
+//                     return false;
+//                }
+//             }
+//             else{
+//                 st.count(v[i]>0){
+//                     return false;   
+//                 }
+//                 m[pattern[i]=v[i]];
+//                 st.insert( v[i]);
+//             }
+//         }
+//         return true;
+//     }
+// };
+
+class Solution {
+public:
+	bool wordPattern(string pattern, string s) {
+		vector<string> v;
+		int i = 0;
+		string temp = "";
+		while(i < s.size()){
+			if(s[i] == ' '){
+				v.push_back(temp);
+				temp = "";
+			}
+			else{
+				temp += s[i];
+			}
+			i++;
+		}
+		v.push_back(temp);
+
+		if(v.size() != pattern.size()){
+			return false;
+		}
+		unordered_map<char, string> m;
+		set<string> st;
+		for(int i = 0; i < pattern.size(); i++){
+			if(m.find(pattern[i]) != m.end()){ //pattern in  hashmap
+                if(m[pattern[i]] != v[i]){
+					return false;
+				}
+			}
+			else{
+				if(st.count(v[i]) > 0){
+					return false;
+				}
+				m[pattern[i]] = v[i];
+				st.insert(v[i]);
+			}
+		}
+		return true;
+	}
+};
